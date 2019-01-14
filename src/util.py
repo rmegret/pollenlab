4f4e97ee1ae3d72ce36536e619dbb802ca8a87c8
Ivan Felipe Rodriguez <ivan.rodriguez5@upr.edu>
2019-01-14 12:38:46 -0400

    Update util.py
    Adding function to load model

--- src/util.py
+++ src/util.py
@@ -1,4 +1,8 @@
 import numpy as np 
+import pandas as pd
+import json 
+from keras.models import model_from_json
+
 from skimage import io, transform
 def dataset(file_list,size=(300,180),flattened=False):
 	data = []
@@ -13,3 +17,15 @@ def dataset(file_list,size=(300,180),flattened=False):
 	labels = [1 if f.split("/")[-1][0] == 'P' else 0 for f in file_list]
 
 	return np.array(data), np.array(labels)
+
+
+def load_json(filename):
+	with open(filename) as input_file:
+		loaded= json.load(input_file)
+	return loaded 
+
+def load_model(json_file,weights_file):
+	loaded_model = load_json(json_file)
+	model=model_from_json(loaded_model)
+	model.load_weights(weights_file)
+	return model
